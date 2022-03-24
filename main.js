8ea5f3dee65854315eb33eaac4278135d249cfda
Gustavo uhlick <tutauhlick@gmail.com>
2022-03-23 22:49:57 -0300

    Nav Bar & Layout Changes

---
+++ main.js
@@ -0,0 +1,20 @@
+/* abre e fecha o menu quando clickar no icone do nav*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+
+for(const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/* quando clickar em um itemd o menu, fechar menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
