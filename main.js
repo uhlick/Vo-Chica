2a9b04e300d41baea4b2366ebf5d57c5d9de0a92
Gustavo uhlick <tutauhlick@gmail.com>
2022-04-08 20:35:12 -0300

    Footer
    I did a footer and ajusts

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-
 for(const element of toggle) {
     element.addEventListener('click', function() {
         nav.classList.toggle('show')
