8c1b68e780874133e526cbebfd264bf6bf24daf0
Gustavo uhlick <tutauhlick@gmail.com>
2022-05-25 17:38:06 -0300

    Finished site

---
+++ main.js
@@ -0,0 +1,9 @@
+/* scrollreveal: mostrar elementos quando der scroll */
+const sr = ScrollReveal();
+
+sr.reveal('.toggle, .first-content-smartphone, .logo, .first-content-desktop, .column3, .column4, .column2, .column1', {
+origin: 'top',
+distance: '20px',
+duration: 1000,
+reset: false
+    })
